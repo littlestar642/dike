285c3624fbf1e85d8ee08ef5485f2351c2d301c9
Parthav Patel <parthav46@gmail.com>
2021-09-28 00:13:09 +0530

    Client changes: - Nodemon support added - Removed backend code

--- client/index.ts
+++ client/index.ts
@@ -1,10 +1,8 @@
 const express = require('express');
 const { default: GetConfig } = require('./src/utils/config');
-const https = GetConfig("SERVER:HTTPS") ? require('https') : require('http');
 const app = express();
 
-const host = GetConfig("SERVER:HOST");
-const port = GetConfig("SERVER:PORT");
+const port = process.env.PORT || GetConfig("SERVER:PORT");
 const devServerEnabled = (process.env.ENV || "development") == "development";
 
 // Webpack dependencies
@@ -13,16 +11,13 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./webpack.config.js');
 
-// API dependencies
-const { default: api } = require('./src/api/controller');
-
 // Web dependencies
 const { default: web } = require('./src/ui/controller');
 
 // React webpack
 if (devServerEnabled) {
 
-    config.forEach(module => {
+    config.forEach((module: any) => {
         let appName = module.devServer.publicPath + "/" + module.output.filename.substr(0, module.output.filename.length - 3);
         module.entry.app.unshift('webpack-hot-middleware/client?reload=true&timeout=1000&path=/__webpack_hmr_' + appName);
         module.plugins.push(new webpack.HotModuleReplacementPlugin());
@@ -47,13 +42,9 @@ if (devServerEnabled) {
 // Static
 app.use(express.static('./public'));
 
-// API
-app.use('/api/', api);
-
 // Web
 app.use('/', web);
 
-const server = https.createServer(app);
-server.listen(port, host, () => {
-    console.log('Server started on: ' + (GetConfig("SERVER:HTTPS") ? 'https://' : 'http://') + host + ':' + port);
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
 });
