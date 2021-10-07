fa29e7c2d8633a78149a28480e3af14f39779b2d
avinash.jaiswal <avinash.jaiswal@gojek.com>
2021-10-07 23:33:06 +0530

    Add redirect url

--- server/controller/default.controller.js
+++ server/controller/default.controller.js
@@ -2,6 +2,11 @@ const Ping = (req, res) => {
     res.send("Hello from AA sample app");
 }
 
+const Redirect = (req,res)=>{
+    res.send("Redirecting...")
+}
+
 module.exports = {
-    Ping
+    Ping,
+    Redirect
 }
